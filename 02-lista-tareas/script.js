85774bff47b954b1ad26c53e43a2986fca340ad6
Oski_Home <david2005prieto@gmail.com>
2026-07-22 18:17:37 -0500

    Implement task creation functionality and enhance input styles for better usability

--- 02-lista-tareas/script.js
+++ 02-lista-tareas/script.js
@@ -2,8 +2,43 @@ console.log("Holaaaaaa juanda")
 
 const button = document.getElementById("add-task-button")
 
-button.onclick = sayHi
+const form = document.getElementById("add-task-container")
+
+const todoList = document.getElementById("todo-list")
+
+const addTaskInput = document.getElementById("add-task-input")
+
+form.onsubmit = createTask
+
+// button.onclick = createTask
 
 function sayHi() {
     console.log("Soy un boton")
+}
+
+function createTask(event) {
+    event.preventDefault()
+    console.log(event)
+    console.log("Default prevented:",event.defaultPrevented)
+    
+    const li = document.createElement("li")
+    const label = document.createElement("label")
+    const input = document.createElement("input")
+    const span = document.createElement("span")
+
+    const taskName = addTaskInput.value
+
+    input.type = "checkbox"
+    
+    span.textContent = taskName
+
+    label.append(input, span)
+
+    li.append(label)
+
+    todoList.append(li)
+
+    addTaskInput.value = ""
+
+    console.log("New task created:",taskName)
 }
